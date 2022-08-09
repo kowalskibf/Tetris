09963419e405b6fcda65cf93301c66cd9d0d5a4f
Bartosz Kowalski <kowalskibf@gmail.com>
2022-08-09 16:51:14 +0200

    Removed Nextblock class

--- main.py
+++ main.py
@@ -5,13 +5,12 @@ from classes.block import Block
 from classes.controls import Controls
 from classes.game import Game
 from classes.interface import Interface
-from classes.nextblock import Nextblock
 
 def main():
     controls = Controls()
     interface = Interface()
     game = Game(10, 20)
-    nextblock = Nextblock(randint(1, 7), 1, randint(1, 6))
+    nextblock = Block(randint(1, 7), 1, randint(1, 6), 4, 0)
     block = Block(randint(1, 7), 1, randint(1, 6), 4, 0)
     keyboard.on_press(controls.on_key_press_out(block, nextblock, game, interface))
     interface.render(game.get_field(), block.get_coords(), block.get_color())
@@ -22,15 +21,14 @@ def main():
             game.solidify_block(block.get_coords(), block.get_color())
             interface.render(game.get_field(), block.get_coords(), block.get_color())
             if game.remove_rows():
-                sleep(1)
                 interface.render(game.get_field(), block.get_coords(), block.get_color())
             del block
-            block = Block(nextblock.get_variant(), nextblock.get_rotation(), nextblock.get_color(), 4, 0)
-            keyboard.on_press(controls.on_key_press_out(block, nextblock, game, interface))
-            del nextblock
-            nextblock = Nextblock(randint(1, 7), 1, randint(1, 6))
+            block = nextblock
             if not game.check_running(block.get_coords()):
                 running = False
+            keyboard.on_press(controls.on_key_press_out(block, nextblock, game, interface))
+            del nextblock
+            nextblock = Block(randint(1, 7), 1, randint(1, 6), 4, 0)
         interface.render(game.get_field(), block.get_coords(), block.get_color())
 
 if __name__ == "__main__":
